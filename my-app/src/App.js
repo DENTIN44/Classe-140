44dfcad4c6f89044f10062ecda32992901d201c5
DENTIN44 <silvaalvg@gmail.com>
2025-02-25 10:58:58 -0400

    im tired of this

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,10 @@
-// src/App.js
 import React, { useState } from 'react';
-import Calculator from './components/Calculator'; // Import Calculator component
+// import Calculator from './components/Calculator'; 
 import StatusUpdate from './components/StatusUpdate'; // Import StatusUpdate component
+import First from './components/First'; // Import First component
+import Second from './components/Second'; // Import Second component
+import Third from './components/Third'; // Import Third component
+import Fourth from './components/Fourth'; // Import Third component
 import './App.css';
 
 function App() {
@@ -14,22 +17,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Calculator App</h1>
+        <h1>React App</h1>
 
         {/* Button to toggle Calculator visibility */}
         <button onClick={toggleCalculator} style={{ marginBottom: '20px', padding: '10px' }}>
-          {showCalculator ? 'Hide' : 'Show'} Calculator
+          {showCalculator ? 'Hide' : 'Show'}
         </button>
 
         {/* Conditionally render Calculator */}
-        {showCalculator && <Calculator />}
+        {/* {showCalculator && <Calculator />} */}
 
         {/* Status Update */}
         <StatusUpdate />
+
+        {/* Additional components for CodePoint */}
+        <First />
+        <Second />
+
+        {/* Add Third component here */}
+        <Third />
+
+        <Fourth />
       </header>
     </div>
   );
 }
 
 export default App;
-    
